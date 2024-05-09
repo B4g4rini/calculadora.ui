6446b6b05aa2e29f532e8905a6df583759b58461
DARK GHOST <pedro.bagarini2000@gmail.com>
2024-05-09 10:08:28 -0300

    Alterações do histórico e resultado

--- Calculadora/Calculadora.xaml.cs
+++ Calculadora/Calculadora.xaml.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace Calculadora;
 
 public partial class Calculadora : ContentPage
@@ -6,24 +8,62 @@ public partial class Calculadora : ContentPage
     {
         InitializeComponent();
     }
-
-    private void insertNum_Clicked(object sender, EventArgs e)
+    int contador = 0;
+    private void calc_Clicked(object sender, EventArgs e)
     {
         Button b = (Button)sender;
         float f = float.TryParse(b.Text, out float res) ? res : 0;
 
-        lblResultado.Text = lblResultado.Text == "0" ? f.ToString() : lblResultado.Text + f.ToString();
+        if (f != 0)
+        {
+            lblResultado.Text = lblResultado.Text == "0" ? f.ToString() : lblResultado.Text + f.ToString();
+            if (contador >= 1)
+                result_Clicked();
+
+        }
+        else
+        {
+            if (lblResultado.Text.Substring(lblResultado.Text.Length - 1) == "+" ||
+                lblResultado.Text.Substring(lblResultado.Text.Length - 1) == "×" ||
+                lblResultado.Text.Substring(lblResultado.Text.Length - 1) == "÷" ||
+                lblResultado.Text.Substring(lblResultado.Text.Length - 1) == "–")
+            {
+                lblResultado.Text = lblResultado.Text.Substring(0, lblResultado.Text.Length - 1)
+                    + b.Text;
+            }
+            else
+            {
+                lblResultado.Text += b.Text;
+                contador++;
+            }
+        }
 
 
+    }
+    private void result_Clicked()
+    {
+        var calcular = lblResultado.Text.Replace("×", "*").Replace("÷", "/").Replace("–", "-");
+        double resultado = Convert.ToDouble(new DataTable().Compute(calcular, null));
+
+        lblHistorico.Text = resultado.ToString();
+    }
+
+    private void btnIgual_Clicked(object sender, EventArgs e)
+    {
+        if (lblHistorico.Text != "")
+        {
+        lblResultado.Text = lblHistorico.Text;
+        lblHistorico.Text = "";
+    }
     }
 
     private void btnCancel_Clicked(object sender, EventArgs e)
     {
         Button b = (Button)sender;
 
-        if (b == btnAC)
+        if (b == btnAC || b == btnC)
         {
-            lblHistorico.Text = "0";
+            lblHistorico.Text = "";
             lblResultado.Text = "0";
         }
         else if (b == btnC)
@@ -42,13 +82,21 @@ public partial class Calculadora : ContentPage
             }
         }
     }
-
     private void btnDecimal_Clicked(object sender, EventArgs e)
     {
         if (lblResultado.Text.Contains(".") is false)
         {
             lblResultado.Text += ".";
         }
+
+    }
+    private void insertNum_Clicked(object sender, EventArgs e)
+    {
+        Button b = (Button)sender;
+        float f = float.TryParse(b.Text, out float res) ? res : 0;
+
+        lblResultado.Text = lblResultado.Text == "0" ? f.ToString() : lblResultado.Text + f.ToString();
+
 
     }
 
